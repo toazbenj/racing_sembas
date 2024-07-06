3df7846b9d2c4fc19fbfa59ce2566726917acacd
thomj130 <thomj130@my.erau.edu>
2024-07-06 13:46:15 -0400

    Tested 3D

--- src/main.rs
+++ src/main.rs
@@ -27,12 +27,12 @@ mod structs;
 mod utils;
 
 const RADIUS: f64 = 1.0;
-const CENTER: SVector<f64, 2> = vector![0.0, 0.0];
-const DOMAIN: Domain<2> = Domain {
-    low: vector![-2.0, -2.0],
-    high: vector![2.0, 2.0],
+const CENTER: SVector<f64, 3> = vector![0.0, 0.0, 0.0];
+const DOMAIN: Domain<3> = Domain {
+    low: vector![-2.0, -2.0, -2.0],
+    high: vector![2.0, 2.0, 2.0],
 };
-fn classify(p: SVector<f64, 2>) -> Result<bool, AdhererError<2>> {
+fn classify(p: SVector<f64, 3>) -> Result<bool, AdhererError<3>> {
     if !DOMAIN.contains(p) {
         return Err(AdhererError::OutOfBoundsError(
             p,
@@ -47,46 +47,17 @@ fn main() {
     test_visual();
 }
 
-fn test() {
-    let d = 0.1;
-    let b0 = vector![0.0, 1.0];
-    let n0 = vector![0.0, 1.0];
-    let v = vector![1.0, 0.0];
-
-    let pivot = Halfspace { b: b0, n: n0 };
-    let delta_angle = 10.0f64.to_radians();
-
-    let adhf = ConstantAdhererFactory::new(d, delta_angle, Some(PI), classify);
-
-    let mut exp = MeshExplorer::new(d, pivot, d * 0.9, Box::new(adhf));
-
-    match exp.step() {
-        Ok(x) => match x {
-            Some(node) => println!("Got result: {}", node),
-            None => println!("End of exploration?"),
-        },
-        Err(e) => println!("Got error: {}", e),
-    }
-}
-
-fn test_graph() {
-    let mut g = Graph::new();
-
-    let a = g.add_node("A");
-    let b = g.add_node("B");
-    g.add_edge(a, b, "edge from a to b");
-
-    println!("Nodes: {}, {}", a.index(), b.index());
-}
-
-fn convert_boundary_to_vec(boundary: &Vec<Halfspace<2>>) -> Vec<(f64, f64)> {
-    return boundary.iter().map(|&hs| (hs.b[0], hs.b[1])).collect();
+fn convert_boundary_to_vec(boundary: &Vec<Halfspace<3>>) -> Vec<(f64, f64, f64)> {
+    return boundary
+        .iter()
+        .map(|&hs| (hs.b[0], hs.b[1], hs.b[2]))
+        .collect();
 }
 
 fn test_visual() {
-    let d = 0.1;
-    let b0 = vector![0.0, 1.0];
-    let n0 = vector![0.0, 1.0];
+    let d = 0.25;
+    let b0 = vector![0.0, 1.0, 0.0];
+    let n0 = vector![0.0, 1.0, 0.0];
 
     let pivot = Halfspace { b: b0, n: n0 };
     let delta_angle = 10.0f64.to_radians();
@@ -99,7 +70,7 @@ fn test_visual() {
 
     let mut frame_number = 0;
 
-    let root_area = BitMapBackend::gif("images/output.gif", (800, 600), 250)
+    let root_area = BitMapBackend::gif("images/output.gif", (800, 600), 100)
         .unwrap()
         .into_drawing_area();
 
@@ -122,20 +93,13 @@ fn test_visual() {
     while let Ok(Some(node)) = exp.step() {
         // println!("Sample: {}", node)
         root_area.fill(&WHITE).unwrap();
-        root_area
-            .draw(&Text::new(
-                format!("{}", 10 - frame_number),
-                (400, 300),
-                ("sans-serif", 80),
-            ))
-            .unwrap();
 
         let mut chart = ChartBuilder::on(&root_area)
             .caption("Adherence Process", ("sans-serif", 50))
-            .build_cartesian_2d(-2.0..2.0, -2.0..2.0)
+            .build_cartesian_3d(-2.0..2.0, -2.0..2.0, -2.0..2.0)
             .unwrap();
 
-        chart.configure_mesh().draw().unwrap();
+        // chart.configure_mesh().draw().unwrap();
 
         chart
             .draw_series(PointSeries::of_element(
@@ -150,7 +114,7 @@ fn test_visual() {
 
         chart
             .draw_series(PointSeries::of_element(
-                vec![(b0[0], b0[1]), (node.p[0], node.p[1])],
+                vec![(b0[0], b0[1], b0[2]), (node.p[0], node.p[1], node.p[2])],
                 5,
                 &BLUE,
                 &|c, s, st| {
@@ -160,7 +124,7 @@ fn test_visual() {
             .unwrap();
 
         root_area.present().unwrap();
-        if frame_number > 100 {
+        if frame_number > 500 {
             break;
         }
         // sleep(Duration::from_millis(500));
