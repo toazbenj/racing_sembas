0b609dd07d7f2ff0fbbe175dc8bda68deee10c77
thomj130 <thomj130@my.erau.edu>
2024-07-06 14:42:35 -0400

    Added global search monte carlo.

---
+++ src/search/global.rs
@@ -0,0 +1,33 @@
+use nalgebra::SVector;
+use rand::{rngs::ThreadRng, Rng};
+
+use crate::structs::Domain;
+
+pub struct MonteCarloSearch<const N: usize> {
+    rng: ThreadRng,
+    domain: Domain<N>,
+}
+
+pub trait SearchFactory<const N: usize> {
+    fn sample(&mut self) -> SVector<f64, N>;
+    fn get_domain(&self) -> &Domain<N>;
+}
+
+impl<const N: usize> MonteCarloSearch<N> {
+    pub fn new(domain: Domain<N>) -> Self {
+        let rng = rand::thread_rng();
+        return MonteCarloSearch { rng, domain };
+    }
+}
+
+impl<const N: usize> SearchFactory<N> for MonteCarloSearch<N> {
+    fn sample(&mut self) -> SVector<f64, N> {
+        let mut v: SVector<f64, N> = SVector::from_fn(|_, _| self.rng.gen());
+        v = v.component_mul(&self.domain.dimensions()) + self.domain.low;
+        return v;
+    }
+
+    fn get_domain(&self) -> &Domain<N> {
+        return &self.domain;
+    }
+}
