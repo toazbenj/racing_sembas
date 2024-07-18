09c7ce6a5f349fe6b73d644a11e47c7f2fd33f5f
thomj130 <thomj130@my.erau.edu>
2024-07-17 21:52:22 -0400

    Made api for remote classifier an optional feature.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 pub mod adherer_core;
 pub mod adherers;
-pub mod api;
 pub mod explorer_core;
 pub mod explorers;
 mod extensions;
 pub mod search;
 pub mod structs;
 mod utils;
+
+#[cfg(feature = "api")]
+pub mod api;
