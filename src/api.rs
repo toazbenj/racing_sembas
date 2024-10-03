28e3b5a35033129b4b5585bdbdb0ad989336ff8a
thomj130 <thomj130@my.erau.edu>
2024-10-03 14:21:58 -0400

    Updated RemoteClassifier to send raw bytes instead of JSON

--- src/api.rs
+++ src/api.rs
@@ -1,29 +1,13 @@
 use crate::structs::SamplingError;
 use nalgebra::SVector;
-use serde::{Deserialize, Serialize};
 use std::io::Write;
 use std::io::{self, Read};
 use std::net;
 
+use crate::structs::Classifier;
 use crate::structs::Domain;
-use crate::{structs::Classifier, utils::svector_to_array};
 
-const BUFFER_SIZE: usize = 2usize.pow(10);
-
-#[derive(Serialize)]
-struct ClassifierRequest {
-    pub p: Vec<f64>,
-}
-
-#[derive(Deserialize)]
-struct ClassifierResponse {
-    pub cls: bool,
-}
-
-#[derive(Deserialize)]
-struct Config {
-    num_params: usize,
-}
+const BUFFER_CONFIG_SIZE: usize = 8;
 
 /// A means to allow an external function under test to connect to SEMBAS and request
 /// where to sample next. The classifier can then be called just like any other
@@ -42,7 +26,7 @@ impl<const N: usize> RemoteClassifier<N> {
     /// 1. RemoteClassifier binds to TcpListener.
     /// 2. FUT connects to socket.
     /// 3. RemoteClassifier accepts connection.
-    /// 4. FUT sends config containing { num_params } config info
+    /// 4. FUT sends config containing num_params config info
     /// 5. RemoteClassifier accepts configuration, throwing error if N != num_params
     /// 6. RemoteClassifier returns OK
     /// 7. RemoteClassifier setup complete, ready to classify.
@@ -53,19 +37,11 @@ impl<const N: usize> RemoteClassifier<N> {
         println!("Connection established.");
 
         println!("Waiting for sim config...");
-        let mut buffer = [0u8; BUFFER_SIZE];
-        let n = stream.read(&mut buffer)?;
-        let config: Config = serde_json::from_slice(&buffer[..n]).map_err(|_| {
-            stream
-                .write_all("ERROR\n".as_bytes())
-                .expect("Invalid 'ERROR' write to stream?");
-            io::Error::new(
-                io::ErrorKind::InvalidData,
-                "Received malformed config data.",
-            )
-        })?;
+        let mut buffer = [0u8; BUFFER_CONFIG_SIZE];
+        stream.read_exact(&mut buffer)?;
+        let num_params = usize::from_be_bytes(buffer);
 
-        if config.num_params != N {
+        if num_params != N {
             stream
                 .write_all(format!("{N}\n").as_bytes())
                 .expect("Invalid N write to stream?");
@@ -74,7 +50,7 @@ impl<const N: usize> RemoteClassifier<N> {
                 io::ErrorKind::InvalidInput,
                 format!(
                     "Invalid number of param names! Expected {N}, Got {}",
-                    config.num_params
+                    num_params
                 ),
             ));
         }
@@ -83,7 +59,7 @@ impl<const N: usize> RemoteClassifier<N> {
             .write_all("OK\n".as_bytes())
             .expect("Invalid 'OK' write to stream?");
 
-        println!("Got config. Ready");
+        println!("Got valid config. Ready.");
 
         let domain = Domain::<N>::normalized();
         Ok(RemoteClassifier { stream, domain })
@@ -114,23 +90,18 @@ impl<const N: usize> Classifier<N> for RemoteClassifier<N> {
         }
 
         // Send request
-        let v: Vec<f64> = svector_to_array(*p).to_vec();
-        let r = ClassifierRequest { p: v };
-        let json = serde_json::to_string(&r).expect("Invalid ClassifierRequest serialization?");
-        self.stream
-            .write_all((json + "\n").as_bytes())
-            .expect("Failed to send classification request to client.");
+        let bytes: &[u8] = bytemuck::cast_slice(p.as_slice());
+        self.stream.write_all(bytes)?;
 
-        // Receive response
-        let mut buffer = [0u8; BUFFER_SIZE];
-        let n = self.stream.read(&mut buffer)?;
-        let ClassifierResponse { cls } = serde_json::from_slice(&buffer[..n]).map_err(|_| {
-            SamplingError::InvalidClassifierResponse(
-                "Received invalid response from client.".to_string(),
-            )
-        })?;
-
-        Ok(cls)
+        let mut buffer = [0; 1];
+        self.stream.read_exact(&mut buffer)?;
+        if buffer[0] > 1 {
+            Err(SamplingError::InvalidClassifierResponse(
+                "Remote Classifier received non-bool response?".to_string(),
+            ))
+        } else {
+            Ok(buffer[0] == 1)
+        }
     }
 }
 
