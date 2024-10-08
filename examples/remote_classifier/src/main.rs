852ef2e1f1c418c0ec6b16819ac16b34686ff397
thomj130 <thomj130@my.erau.edu>
2024-10-07 20:17:31 -0400

    Updated remote classifier example

--- examples/remote_classifier/src/main.rs
+++ examples/remote_classifier/src/main.rs
@@ -7,14 +7,22 @@ use std::{
 use nalgebra::SVector;
 use sembas::{
     api::RemoteClassifier,
-    structs::{Classifier, Domain, SamplingError},
+    sps::Sphere,
+    structs::{Classifier, Domain},
 };
-use serde::{Deserialize, Serialize};
-use serde_json::json;
 
 /// The number of dimensions that the UAS simulation has.
 const D: usize = 7;
+const SAMPLE_SIZE: usize = D * 8;
 
+/// This example is to illustrate how the RemoteClassifier works. Since this requires
+/// two independent processes, we will be using a thread for both the SEMBAS program
+/// and the Function Under Test (FUT).
+/// Normally, you would *not* use the RemoteClassifier unless you cannot import
+/// sembas crate. However, to keep things in one program we will have both the FUT
+/// and the SEMBAS test solution in the same program.
+/// If you have an application in a Python script, or other such programming
+/// language, this can act as a versatile bridge between applications.
 fn main() {
     // First start your SEMBAS program:
     let local_program_handle = setup_local();
@@ -38,18 +46,12 @@ fn setup_local() -> JoinHandle<()> {
 
         let p1 = SVector::from_fn(|_, _| 0.5);
         println!("[Local] Sending {p1:?} request");
-        let r = classifier
-            .classify(&p1)
-            .inspect_err(|e| println!("Error: {e:?}"))
-            .unwrap();
+        let r = classifier.classify(&p1).unwrap();
         println!("[Local] Received result of {r}");
 
         let p2 = SVector::zeros();
         println!("[Local] Sending {p1:?} request");
-        let r = classifier
-            .classify(&p2)
-            .inspect_err(|e| println!("Error: {e:?}"))
-            .unwrap();
+        let r = classifier.classify(&p2).unwrap();
         println!("[Local] Received result of {r}");
     })
 }
@@ -60,10 +62,11 @@ fn setup_remote<const N: usize>() -> JoinHandle<()> {
         let mut socket = TcpStream::connect("127.0.0.1:2000").unwrap();
         println!("[Remote] Connection established!");
 
-        println!("[Remote] Sending config...");
-        let config = json!({ "num_params": N });
-        socket.write_all(config.to_string().as_bytes()).unwrap();
+        println!("[Remote] Sending config (dimension size)...");
+        let config = N.to_be_bytes();
+        socket.write_all(&config).unwrap();
 
+        println!("[Remote] Waiting for OK...");
         let mut buffer = [0u8; 1024];
         let n = socket.read(&mut buffer).unwrap();
 
@@ -71,70 +74,55 @@ fn setup_remote<const N: usize>() -> JoinHandle<()> {
             .expect("Got bad message?")
             .to_string();
 
-        println!("Got acknowledgement of {ack} back.");
+        println!("Got acknowledgement of {:?} back.", ack.trim());
 
         if ack.trim() != "OK" {
             println!("[Remote] Invalid configuration, exiting.");
             return;
         }
-
-        // This would be some function that you're testing that has some well-defined
-        // performance mode. Example: an AV test scenario that runs, a sim summary is
-        // returned, which you parse and return true if a collision occurred
-        // otherwise false.
-        let mut classifier: Box<dyn Classifier<N>> = Box::new(Sphere {
-            center: SVector::<f64, N>::from_fn(|_, _| 0.5),
-            radius: 0.25,
-            domain: Domain::normalized(),
-        });
-
         println!("[Remote] Configuration accepted. Waiting for requests.");
 
-        loop {
-            let mut buffer = [0u8; 1024];
-            let n = socket.read(&mut buffer).unwrap();
-            match serde_json::from_slice(&buffer[..n]) {
-                Ok(ClassifierRequest { p }) => {
-                    let p = SVector::<f64, N>::from_fn(|i, _| p[i]);
-                    let cls = classifier.classify(&p).unwrap_or(false);
+        // This would be some function that you're testing that has some well-defined
+        // performance mode. Example: an AV test scenario that runs, from which a
+        // summary is returned, which you parse and return true if a collision
+        // occurred otherwise false.
+        let mut classifier: Box<dyn Classifier<N>> = Sphere::boxed(
+            SVector::<f64, N>::from_fn(|_, _| 0.5),
+            0.25,
+            Some(Domain::normalized()),
+        );
 
-                    let msg = serde_json::to_string(&ClassifierResponse { cls }).unwrap();
-                    socket
-                        .write_all((msg + "\n").as_bytes())
-                        .expect("Failed to send classification result.");
+        loop {
+            let mut buffer = [0u8; SAMPLE_SIZE];
+            let n = socket.read(&mut buffer).unwrap();
+
+            // Expecting a N*8 byte sized message, which is the SVector<f64, N>
+            if n < SAMPLE_SIZE {
+                // In case it is not, then it's a termination signal or error
+                let msg = std::str::from_utf8(&buffer[..n])
+                    .expect("Received unexpected response from server")
+                    .to_string();
+
+                if msg.trim() == "end" {
+                    println!("[Remote] SEMBAS Completed, ending session.");
+                    break;
+                } else {
+                    panic!("Got unexpected message: {:?}.", msg.trim());
                 }
-                Err(_) => return, // terminate when 'end' is received instead of request
             }
+
+            let p: SVector<f64, N> = SVector::from_row_slice(
+                bytemuck::try_cast_slice::<u8, f64>(&buffer)
+                    .expect("Incorrect byte size or alignment"),
+            );
+
+            let cls = classifier.classify(&p).unwrap_or(false) as u8;
+
+            socket.write_all(&[cls]).unwrap();
         }
     })
 }
 
 fn setup_classifier() -> Box<dyn Classifier<D>> {
     Box::new(RemoteClassifier::bind("127.0.0.1:2000".to_string()).expect("Failed to connect"))
-}
-
-struct Sphere<const N: usize> {
-    pub radius: f64,
-    pub center: SVector<f64, N>,
-    pub domain: Domain<N>,
-}
-
-impl<const N: usize> Classifier<N> for Sphere<N> {
-    fn classify(&mut self, p: &SVector<f64, N>) -> Result<bool, SamplingError<N>> {
-        if !self.domain.contains(p) {
-            return Err(SamplingError::OutOfBounds);
-        }
-
-        Ok((p - self.center).norm() <= self.radius)
-    }
-}
-
-#[derive(Deserialize)]
-struct ClassifierRequest {
-    pub p: Vec<f64>,
-}
-
-#[derive(Serialize)]
-struct ClassifierResponse {
-    pub cls: bool,
 }
