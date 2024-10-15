1e26bd6ee86015bfa12d3d6bd03709002ac69566
thomj130 <thomj130@my.erau.edu>
2024-10-15 18:12:28 -0400

    Added get_rtree_from_boundary for constructing RTree

--- src/boundary_tools/mod.rs
+++ src/boundary_tools/mod.rs
@@ -1,1 +1,18 @@
+use crate::prelude::{Boundary, BoundaryRTree, Halfspace, KnnNode};
+use rstar::RTree;
 
+/// Converts a boundary into an RTree. This is useful when many K-nearest neighbor
+/// searches are needed.
+/// ## Arguments
+/// * boundary : The boundary to be placed within a RTree structure.
+/// ## Return
+/// * boundary_rtree : BoundaryRTree (RTree<GeomWithData<[f64; N], usize>>)
+pub fn get_rtree_from_boundary<const N: usize>(boundary: &Boundary<N>) -> BoundaryRTree<N> {
+    RTree::bulk_load(
+        boundary
+            .iter()
+            .enumerate()
+            .map(|(i, hs)| KnnNode::new(hs.b.into(), i))
+            .collect(),
+    )
+}
