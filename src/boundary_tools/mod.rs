543efe99442b6440a9cc7f50ccc8855c50d48189
thomj130 <thomj130@my.erau.edu>
2024-10-15 18:12:57 -0400

    Added falls_on_boundary for determining membership of HS

--- src/boundary_tools/mod.rs
+++ src/boundary_tools/mod.rs
@@ -16,3 +16,42 @@ pub fn get_rtree_from_boundary<const N: usize>(boundary: &Boundary<N>) -> Bounda
             .collect(),
     )
 }
+
+/// Returns true if the provided halfspace @hs is likely to be on the surface of
+/// @boundary. This is an early implementation, and is more of a proof-of-concept
+/// than a robust solution.
+/// ## Warning
+/// * Performs poorly on edges sharp corners of envelopes.
+/// * Performs poorly with low resolution boundaries (i.e. with large  jumpst
+///   distances @d).
+/// * Performs poorly when @d <= min(envelope_diameters)
+/// ## Arguments
+/// * hs : The halfspace to query against @boundary.
+/// * boundary : The known boundary to compare @hs to.
+/// ## Returns
+/// * is_on_boundary : true ifthe halfspace is likely to be on the boundary,
+///   otherwise false. This is an approximation and may incur false positives and
+///   negatives. Accuracy improves with density and completeness of @boundary.
+pub fn falls_on_boundary<const N: usize>(
+    d: f64,
+    hs: &Halfspace<N>,
+    boundary: &Boundary<N>,
+    boundary_rtree: &BoundaryRTree<N>,
+) -> bool {
+    // The maximum distance between two points on the boundary.
+    let max_dist = d * (N as f64).sqrt();
+
+    let node = boundary_rtree
+        .nearest_neighbor(&hs.b.into())
+        .expect("Boundary RTree must not be empty");
+
+    let b_index = node.data;
+    let nearest_hs = boundary.get(b_index).expect("Boundary index from BoundaryRTree node was out of bounds. This can occur if &Boundary is not the boundary same as &BoundaryRTree");
+
+    let dist = (nearest_hs.b - hs.b).norm();
+    if dist > max_dist {
+        false
+    } else {
+        hs.n.dot(&nearest_hs.n) >= 0.0
+    }
+}
