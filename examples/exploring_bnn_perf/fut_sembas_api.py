a7e506732995497f4a3d5c2dee8db4e72c35b494
thomj130 <thomj130@my.erau.edu>
2024-10-17 19:39:52 -0400

    Added wait_until_open to handle server being offline

--- examples/exploring_bnn_perf/fut_sembas_api.py
+++ examples/exploring_bnn_perf/fut_sembas_api.py
@@ -1,16 +1,36 @@
 import socket
 import struct
-import numpy as np
+from time import sleep
 
-from numpy import ndarray
+import torch
 
 
-def setup_socket(n: int):
+def wait_until_open(
+    client: socket.socket, max_attempts: int | None = 10, delay: float = 0.1
+):
+    success = False
+    i = 0
+    while not success and (max_attempts is None or i < max_attempts):
+        try:
+            client.connect(("127.0.0.1", 2000))
+            success = True
+        except:
+            sleep(delay)
+
+        i += 1
+
+
+def setup_socket(ndim: int, fail_on_refuse=False):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(("127.0.0.1", 2000))
+
+    if fail_on_refuse:
+        client.connect(("127.0.0.1", 2000))
+    else:
+        wait_until_open(client)
+
     # send config to remote classifier
     try:
-        ndim_packed = struct.pack("!q", n)
+        ndim_packed = struct.pack("!q", ndim)
         client.sendall(ndim_packed)
 
         msg = client.recv(1024).decode("utf-8")
@@ -24,10 +44,10 @@ def setup_socket(n: int):
     return client
 
 
-def receive_request(client: socket.socket, n: int) -> ndarray:
-    data_size = n * 8
+def receive_request(client: socket.socket, ndim: int) -> torch.Tensor:
+    data_size = ndim * 8  # ndim * size(f64)
     data = client.recv(data_size)
-    return np.array(struct.unpack(f"{n}d", data))
+    return torch.tensor(struct.unpack(f"{ndim}d", data))
 
 
 def send_response(client: socket.socket, cls: bool):
