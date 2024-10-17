e23357e7ec43e1f60a530812b07986e3af4170e9
thomj130 <thomj130@my.erau.edu>
2024-10-17 19:38:38 -0400

    Added transform_request for creating valid inputs

--- examples/exploring_bnn_perf/fut_data.py
+++ examples/exploring_bnn_perf/fut_data.py
@@ -39,6 +39,9 @@ class FutData(Dataset):
             self.target_scaler.fit_transform(self.targets), dtype=torch.float32
         )
 
+        self.input_min = self.inputs_std.min().item()
+        self.input_max = self.inputs_std.max().item()
+
     @property
     def inputs_std(self):
         return self.inputs
@@ -54,6 +57,9 @@ class FutData(Dataset):
     @property
     def targets_nonstd(self):
         return self.target_scaler.inverse_transform(self.targets)
+
+    def transform_request(self, x: Tensor) -> Tensor:
+        return self.input_min + x * (self.input_max - self.input_min)
 
     def __len__(self):
         return self.data_size
