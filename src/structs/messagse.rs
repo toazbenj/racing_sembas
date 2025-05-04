d5a022f75f02c8ade73d4074482a9946137f07b0
thomj130 <thomj130@my.erau.edu>
2025-05-04 12:22:56 -0400

    Added message constants

---
+++ src/structs/messagse.rs
@@ -0,0 +1,6 @@
+pub const MSG_OK: &str = "OK";
+pub const MSG_ERR: &str = "ERROR";
+pub const MSG_END: &str = "END";
+pub const MSG_PHASE_GLOBAL_SEARCH: &str = "GS";
+pub const MSG_PHASE_SURFACE_SEARCH: &str = "SS";
+pub const MSG_PHASE_BOUNDARY_EXPL: &str = "BE";
